50f111fc2cc25a319b6b7583af9028430f3a11ff
Jakedruth@gmail.com <Jakedruth@gmail.com>
2020-10-01 18:32:22 -0400

    Moved data structers into a sub folder for oganization

---
+++ AdvanceAnimationProgramming/Assets/Scripts/DataStructures/HierachicalPose/HierarchyNode.cs
@@ -0,0 +1,15 @@
+﻿/*
+	Advanced Animation Programming
+	By Jake Ruth
+
+    HierarchyNode.cs - class to represent a decoupled node of data in a hierarchy
+*/
+
+
+namespace AdvAnimation
+{
+    public class HierarchyNode
+    {
+
+    }
+}
