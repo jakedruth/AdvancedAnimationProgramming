15c1f3f3e1795d9ab27921e842404e861aead322
Jakedruth@gmail.com <Jakedruth@gmail.com>
2020-10-12 10:13:50 -0400

    Impleemented function Identity

---
+++ AdvanceAnimationProgramming/Assets/Scripts/DataStructures/MathAA.cs
@@ -0,0 +1,19 @@
+﻿
+namespace AdvAnimation
+{
+    public class MathAA
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
