ee6c5a5f750db7a39890751594fbb0bf5062d813
Jakedruth@gmail.com <Jakedruth@gmail.com>
2020-12-10 09:35:16 -0500

    started work on getting the tail thing to move around the ground

--- AdvanceAnimationProgramming/Assets/Scripts/IK/IKTail.cs
+++ AdvanceAnimationProgramming/Assets/Scripts/IK/IKTail.cs
@@ -23,16 +23,18 @@ namespace AdvAnimation
 
         void Update()
         {
-            Ray ray = new Ray(restTransform.position, -restTransform.up);
+            Ray ray = new Ray(restTransform.position + restTransform.up, -restTransform.up);
             Debug.DrawRay(ray.origin, ray.direction, Color.red);
             Vector3 pos = restTransform.position;
+            Quaternion targetRot = locator.rotation;
             if (Physics.Raycast(ray, out RaycastHit hit))
             {
                 pos = hit.point - ray.direction * _height;
-                locator.rotation = MathAA.GetRotationFromRaycastHitAndForward(hit, rootSpace.forward);
+                targetRot = MathAA.GetRotationFromRaycastHitAndForward(hit, restTransform.forward);
             }
 
             locator.position = Vector3.MoveTowards(locator.position, pos, 1.0f * Time.deltaTime);
+            locator.rotation = Quaternion.RotateTowards(locator.rotation, targetRot, 90f * Time.deltaTime);
 
             _proceduralGrab.ResolveIK(locator.position, locator.rotation, rootSpace.up);
         }
